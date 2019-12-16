5bc0ebeb81910f1a50ecae96f9d5732e05725b8f
Dani <daniel.rodriguez@ext.privalia.com>
2019-12-16 12:29:14 +0100

    fix game

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppService } from './services/app.service';
+import { GameService } from './services/game.service';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,10 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    AppService,
+    GameService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
